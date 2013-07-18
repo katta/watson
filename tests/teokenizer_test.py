1cc7efa5ddd14851b8c399b082b75930ddf5dae8
Srivatsa Katta <vatsa.katta@gmail.com>
2013-07-18 18:08:46 +0530

    Tokenizing documents

--- tests/teokenizer_test.py
+++ tests/teokenizer_test.py
@@ -9,3 +9,13 @@ class TokenizerTest(unittest.TestCase):
 		tokens = Tokenizer.tokenize(document)
 
 		self.assertEqual(len(tokens),3)
+
+	def test_tokenize_documents(self):
+		document1 = "Matrix of TelePresence components continue"
+		document2 = "This Table provides a description of various TelePresence components like voip."
+		all_documents = {"doc1":document1,"doc2":document2}
+		tokenized_documents = Tokenizer.tokenize_documents(all_documents)
+
+		self.assertEqual(len(tokenized_documents.keys()),2)
+		self.assertEqual(len(tokenized_documents["doc1"]),3)				
+		self.assertEqual(len(tokenized_documents["doc2"]),4)
--- watson/tokenizer.py
+++ watson/tokenizer.py
@@ -1,6 +1,17 @@
 from watson import TextProcessor
+from collections import defaultdict
+
 class Tokenizer():
 
-	@classmethod
-	def tokenize(self, document):
-		return TextProcessor().extract_nouns(document)
+    @classmethod
+    def tokenize(self, document):
+        return TextProcessor().extract_nouns(document)
+
+    @classmethod
+    def tokenize_documents(self, documents):
+        tokenized_documents = defaultdict(list)
+    	for doc_id, document in documents.iteritems():
+        	tokens = self.tokenize(document)
+        	tokenized_documents[doc_id] = tokens
+
+    	return tokenized_documents
