9f7b2a2e688a56b2d7af02646efd4fe82841b6a1
Srivatsa Katta <vatsa.katta@gmail.com>
2013-07-18 16:10:47 +0530

    Calculating document frequency for tokens - pramod/katta

--- tests/weighing_measure_test.py
+++ tests/weighing_measure_test.py
@@ -6,5 +6,21 @@ class WeighingMeasureTest(unittest.TestCase):
 
     def test_frequency_measure_for(self):
         document_tokens = ["cisco", "router", "cisco", "switch"]
+
         frequencies = WeighingMeasure.frequency_for(document_tokens)
+
         self.assertEqual(len(frequencies.keys()), 3)
+        self.assertEqual(frequencies["cisco"], 2)
+        self.assertEqual(frequencies["router"], 1)
+
+    def test_document_frequncy_measure_for(self):
+        document1_tokens = ["cisco", "router", "cisco", "switch"]
+        document2_tokens = ["network", "router", "cisco", "bridge", "voip"]
+        tokenized_documents = {"doc1": document1_tokens, "doc2": document2_tokens}
+
+        document_frequencies = WeighingMeasure.document_frequency_for(tokenized_documents)
+
+        self.assertEqual(len(document_frequencies.keys()), 6)
+        self.assertEqual(document_frequencies["cisco"], 2)
+        self.assertEqual(document_frequencies["switch"], 1)
+        self.assertEqual(document_frequencies["voip"], 1)
--- watson/weighing_measure.py
+++ watson/weighing_measure.py
@@ -1,9 +1,19 @@
 from collections import defaultdict
+
+
 class WeighingMeasure():
 
     @classmethod
     def frequency_for(self, document_tokens):
-    	term_frequency = defaultdict(long)
-    	for token in document_tokens:
-    		term_frequency[token] += 1
+        term_frequency = defaultdict(long)
+        for token in document_tokens:
+            term_frequency[token] += 1
         return term_frequency
+
+    @classmethod
+    def document_frequency_for(self, tokenized_documents):
+        document_frequency = defaultdict(long)
+        for docid, tokens in tokenized_documents.iteritems():
+            for token in list(set(tokens)):
+                document_frequency[token] += 1
+        return document_frequency
