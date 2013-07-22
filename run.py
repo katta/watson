88c61dc9f75d869258f1076c2c221bcda0ef78f8
Srivatsa Katta <vatsa.katta@gmail.com>
2013-07-22 12:14:42 +0530

    Logging with new line via run.py

--- run.py
+++ run.py
@@ -10,11 +10,10 @@ def run():
 		return -1
 
 	content_root_path = sys.argv[1]
-	# content = Content(content_root_path)
 	tokenized_documents = Tagger.tag(content_root_path)
 
 	for key,value in tokenized_documents.iteritems():
-		print key, ":", value
+		print "\n\nDoc Id [%s] \nTags [%s]" % (key, value)
 
 if __name__ == "__main__" :
 	run()
