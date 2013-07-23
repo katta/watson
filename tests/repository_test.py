941b22ab8c8084a436d377ac619fddc74e2f5e12
Srivatsa Katta <vatsa.katta@gmail.com>
2013-07-23 18:06:11 +0530

    Created repository entity to hold on to all documents

--- tests/repository_test.py
+++ tests/repository_test.py
@@ -1,6 +1,35 @@
 import unittest
-from watson import Document
+from watson import Document, Repository
+
 
 class RepositoryTest(unittest.TestCase):
-	def test_add_document(self):
-		pass
+
+    def setUp(self):
+        self.rep = Repository()
+
+    def test_add_document(self):
+        doc1 = Document("d1", "doc 1 text")
+
+        self.rep.add(doc1)
+
+        self.assertEqual(len(self.rep.all_docs()), 1)
+
+        self.rep.add(Document("d2", "doc 2 text"))
+        self.assertEqual(len(self.rep.all_docs()), 2)
+
+    def test_add_only_if_does_not_exist(self):
+        self.rep.add(Document("d1", "doc 1 text"))
+
+        with self.assertRaises(RuntimeError):
+            self.rep.add(Document("d1", "doc 1 text changed"))
+
+    def test_find_doc_by_id(self):
+        self.rep.add(Document("d1", "doc 1 text"))
+        self.rep.add(Document("d2", "doc 2 text"))
+
+        doc1 = self.rep.find_doc_by_id("d1")
+        doc2 = self.rep.find_doc_by_id("d2")
+
+        self.assertEqual(doc1.id(), "d1")
+        self.assertEqual(doc2.text(), "doc 2 text")
+        self.assertTrue(self.rep.find_doc_by_id("someid") is None)
--- watson/repository.py
+++ watson/repository.py
@@ -2,3 +2,15 @@ class Repository():
 
     def __init__(self):
         self.all_documents = []
+
+    def add(self, document):
+		for doc in self.all_documents:
+			if doc.id() is document.id():
+				raise RuntimeError("Document with %s already exists in Repository" % (doc.id()))
+		self.all_documents.append(document)
+
+    def all_docs(self):
+        return self.all_documents
+
+    def find_doc_by_id(self, doc_id):
+    	return next((d for d in self.all_documents if d.id() is doc_id) , None)
